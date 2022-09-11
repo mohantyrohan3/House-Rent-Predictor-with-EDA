1c667adca952fd607b195c81d5f1df236cf4f3a7
Rohan Mohanty <mohantyrohan3@gmail.com>
2022-09-11 16:03:44 +0530

    make it better

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify , render_template
 import util
-application = Flask(__name__)
-@application.route('/')
+app = Flask(__name__)
+@app.route('/')
 def home():
     util.load_saved_artifacts()
     return render_template('index.html')
 
 
 
-@application.route('/get_location_names')
+@app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
         'locations': util.load_saved_artifacts()
@@ -18,7 +18,7 @@ def get_location_names():
 
 
 
-@application.route('/ahmedabad_model', methods=['POST'])
+@app.route('/ahmedabad_model', methods=['POST'])
 def predict_home_price_ahmedabad():
     bedroom = int(request.form['bedroom'])
     area = float(request.form['area'])
@@ -32,7 +32,7 @@ def predict_home_price_ahmedabad():
     return response
 
 
-@application.route('/Mumbai_model', methods=['POST'])
+@app.route('/Mumbai_model', methods=['POST'])
 def predict_home_price_mumbai():
     bedroom = int(request.form['bedroom'])
     area = float(request.form['area'])
@@ -45,7 +45,7 @@ def predict_home_price_mumbai():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@application.route('/Delhi_model', methods=['POST'])
+@app.route('/Delhi_model', methods=['POST'])
 def predict_home_price_delhi():
     bedroom = int(request.form['bedroom'])
     area = float(request.form['area'])
@@ -58,7 +58,7 @@ def predict_home_price_delhi():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@application.route('/Bangalore_model', methods=['POST'])
+@app.route('/Bangalore_model', methods=['POST'])
 def predict_home_price_bangalore():
     bedroom = int(request.form['bedroom'])
     area = float(request.form['area'])
@@ -71,7 +71,7 @@ def predict_home_price_bangalore():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@application.route('/Kolkata_model', methods=['POST'])
+@app.route('/Kolkata_model', methods=['POST'])
 def predict_home_price_kolkata():
     bedroom = int(request.form['bedroom'])
     area = float(request.form['area'])
@@ -89,4 +89,4 @@ def predict_home_price_kolkata():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    application.run(debug=True)
+    app.run(debug=True)
